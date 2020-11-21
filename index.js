8daf5156a9010ab06d4e09cd0571c11d1e049f1d
WinstonMeh <WinstonMeh@users.noreply.github.com>
2020-11-21 13:23:46 +1100

    fix inconsistencies

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const { Plugin } = require("powercord/entities");
-const { inject, uninject } = require("powercord/injector");
-const { getModule } = require("powercord/webpack");
+const { Plugin } = require('powercord/entities');
+const { inject, uninject } = require('powercord/injector');
+const { getModule } = require('powercord/webpack');
 const replyMdl = getModule(['createPendingReply'], false);
 
 module.exports = class NoAutoReplyPing extends Plugin {
     async startPlugin() {
         inject('no-auto-reply-ping', replyMdl, 'createPendingReply', args => {
-            args[0].shouldMention = false
-            return args
-        }, true)
+            args[0].shouldMention = false;
+            return args;
+        }, true);
     }
 
     pluginWillUnload() {
